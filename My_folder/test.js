29322ca375dca200ae5d527eda84f3f03a956945
vishalstar-ux <vm369391@gmail.com>
2024-11-28 17:49:13 +0000

    allok

--- My_folder/test.js
+++ My_folder/test.js
@@ -1,1 +1,19 @@
+const accountId = 123456
+let accountEmail = "vishak123@gmail.com"
+var accountPassword = "123456"
+accountCity = "Delhi"
+
+accountEmail = "Rohitkumar@gmail.com"
+accountPassword = "232332"
+accountCity = "Gova"
+
+/*
+Perfer not to use var 
+because of issue in block  scope and functional = scope
+*/
+
+
+
+
+
 console.log("vishal mandal")
